c58ee24b685da2ce9a4d8428a2decef1202588c6
AndrejaKardos <andreja.kardos@gmail.com>
2020-08-31 12:57:35 +1000

    Added links to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,14 +10,14 @@ class Footer extends Component {
                 </div>
                 <div id='footer-lower'>
                     <span id='footer-lower-left'>
-                        Advertising 
-                        Business
-                        How Search works
+                        <a href='https://ads.google.com/intl/en_au/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2m--ahpm-0000000034-0000000001&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1' className='left-footer-link'>Advertising</a>
+                        <a href='https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1#?modal_active=none' className='left-footer-link'>Business</a>
+                        <a href='https://www.google.com/search/howsearchworks/?fg=1' className='left-footer-link'>How Search works</a>
                     </span>
                     <span id='footer-lower-right'>
-                        Privacy
-                        Terms
-                        Settings
+                        <a href='https://policies.google.com/privacy?hl=en-AU&fg=1' className='right-footer-link'>Privacy</a>
+                        <a href='https://policies.google.com/terms?hl=en-AU&fg=1' className='right-footer-link'>Terms</a>
+                        <a href='https://www.google.com/preferences?hl=en' className='right-footer-link'>Settings</a>
                     </span>
                 </div>
             </div>
