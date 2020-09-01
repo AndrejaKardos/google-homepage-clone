8aa23287aed4ffab3dc650cac971222ffc3b3ec8
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-01 16:04:59 +1000

    Removed redundant code in Email.js

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -6,38 +6,28 @@ import '../App.css'
 class Email extends Component {
     render() {
         return (
-            <div id='email-container'>
-                <div className='email-account-div'>
-                    <a href='https://www.google.com/webhp?authuser=1' className='email-account'>
-                        <div className='email-icon'>
+            <div className='email-account-div'>
+                    <div className='email-info'>
+                        <div className='email-icon'></div>
                             <FontAwesomeIcon icon={faUser} transform='grow-16' id='icon-user-purple' />
-                            email1@gmail.com
+                        <div className='email-text'>
+                            <div className='email-text-top'>Username</div>
+                            <div className='email-text-bottom'>email3@gmail.com</div>
                         </div>
-                    </a>
-                </div>
-                <div className='email-account-div'>
-                    <a href='https://www.google.com/webhp?authuser=2' className='email-account'>
-                        <div className='email-icon'>
-                            <FontAwesomeIcon icon={faUser} transform='grow-16' id='icon-user-green' /> 
-                            email2@gmail.com
-                        </div>
-                    </a>
-                </div>
-                <div className='email-account-div'>
-                    <a href='https://www.google.com/webhp?authuser=3' className='email-account'>
-                        <div className='email-icon'>
-                            <FontAwesomeIcon icon={faUser} transform='grow-16' id='icon-user-pink' />
-                            email3@gmail.com
-                        </div>
-                    </a>
-                </div>
-                <div className='email-account-div'>
-                    <a href='https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession' className='email-account'>
-                        <FontAwesomeIcon icon={faUserPlus} transform='grow-5' id='add-user-icon' />
-                        Add another account
-                    </a>
-                </div>
+                    </div>
             </div>
+
+            // {/* <div className='email-account-div'>
+            //     <a href='https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession' className='email-account'>
+            //         <div className='email-info'>
+            //             <div className='email-icon'>
+            //                 <FontAwesomeIcon icon={faUserPlus} transform='grow-5' id='add-user-icon' />
+            //                 Add another account
+            //             </div>
+                        
+            //         </div>
+            //     </a>
+            // </div> */}
         );
     }
 }
