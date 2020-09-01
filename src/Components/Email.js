d2d61822327abf00f14e76d6200539159139e104
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-01 15:47:51 +1000

    Edited user icon colours for variety

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -10,7 +10,7 @@ class Email extends Component {
                 <div className='email-account-div'>
                     <a href='https://www.google.com/webhp?authuser=1' className='email-account'>
                         <div className='email-icon'>
-                            <FontAwesomeIcon icon={faUser} transform='shrink-3.5' id='icon-user-purple' />
+                            <FontAwesomeIcon icon={faUser} transform='grow-16' id='icon-user-purple' />
                             email1@gmail.com
                         </div>
                     </a>
@@ -18,7 +18,7 @@ class Email extends Component {
                 <div className='email-account-div'>
                     <a href='https://www.google.com/webhp?authuser=2' className='email-account'>
                         <div className='email-icon'>
-                            <FontAwesomeIcon icon={faUser} transform='shrink-3.5' id='icon-user-green' /> 
+                            <FontAwesomeIcon icon={faUser} transform='grow-16' id='icon-user-green' /> 
                             email2@gmail.com
                         </div>
                     </a>
@@ -26,14 +26,14 @@ class Email extends Component {
                 <div className='email-account-div'>
                     <a href='https://www.google.com/webhp?authuser=3' className='email-account'>
                         <div className='email-icon'>
-                            <FontAwesomeIcon icon={faUser} transform='shrink-3.5' id='icon-user-pink' />
+                            <FontAwesomeIcon icon={faUser} transform='grow-16' id='icon-user-pink' />
                             email3@gmail.com
                         </div>
                     </a>
                 </div>
                 <div className='email-account-div'>
                     <a href='https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession' className='email-account'>
-                        <FontAwesomeIcon icon={faUserPlus} transform='shrink-3.5' id='add-user-icon' />
+                        <FontAwesomeIcon icon={faUserPlus} transform='grow-5' id='add-user-icon' />
                         Add another account
                     </a>
                 </div>
