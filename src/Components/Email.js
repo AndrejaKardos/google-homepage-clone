5356808b7d073d5b121a177a9143a0a11870d7c2
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-01 15:39:52 +1000

    Added user icons from Font Awesome to e-mail links

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import '../App.css'
 
 class Email extends Component {
@@ -7,17 +9,26 @@ class Email extends Component {
             <div id='email-container'>
                 <div className='email-account-div'>
                     <a href='https://www.google.com/webhp?authuser=1' className='email-account'>
-                        email1@gmail.com
+                        <div className='email-icon'>
+                            <FontAwesomeIcon icon={faUser} transform='shrink-3.5' id='icon-user-purple' />
+                            email1@gmail.com
+                        </div>
                     </a>
                 </div>
                 <div className='email-account-div'>
                     <a href='https://www.google.com/webhp?authuser=2' className='email-account'>
-                        email2@gmail.com
+                        <div className='email-icon'>
+                            <FontAwesomeIcon icon={faUser} transform='shrink-3.5' id='icon-user-green' /> 
+                            email2@gmail.com
+                        </div>
                     </a>
                 </div>
                 <div className='email-account-div'>
                     <a href='https://www.google.com/webhp?authuser=3' className='email-account'>
-                        email3@gmail.com
+                        <div className='email-icon'>
+                            <FontAwesomeIcon icon={faUser} transform='shrink-3.5' id='icon-user-pink' />
+                            email3@gmail.com
+                        </div>
                     </a>
                 </div>
                 <div className='email-account-div'>
