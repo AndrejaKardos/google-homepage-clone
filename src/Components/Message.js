e8427dece1550993aac3325b4c4139c9d05ace9b
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-02 16:21:31 +1000

    Applied BEM naming to Message

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -6,9 +6,9 @@ import './Message.css'
 class Message extends Component {
     render() {
         return (
-            <div id='message-container'>
-                <FontAwesomeIcon icon={faExclamationTriangle} transform='grow-20 left-17' id='icon-warning' />
-                <a href='https://australia.googleblog.com/2020/08/13-things-you-need-to-know-about-news.html' id='message-link'>Get answers</a> on a proposed law that puts Google Search and YouTube at risk #AFairCode                
+            <div id='message'>
+                <FontAwesomeIcon icon={faExclamationTriangle} transform='grow-20 left-17' id='message__icon' />
+                <a href='https://australia.googleblog.com/2020/08/13-things-you-need-to-know-about-news.html' id='message__link'>Get answers</a> on a proposed law that puts Google Search and YouTube at risk #AFairCode                
             </div>
         );
     }
