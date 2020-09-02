f8b5cace2d984e33e44caf8ec8906c16d50171bd
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-02 16:06:15 +1000

    Removed redundant div in Email.js

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -6,17 +6,15 @@ import './Email.css'
 class Email extends Component {
     render() {
         return (
-            <div className='email-account-div'>
-                    <div className='email-info'>
-                        <div className='email-icon'>
-                            <FontAwesomeIcon icon={faUser} transform='grow-20' className='icon-user-secondary' />
-                        </div>
-                            
-                        <div className='email-text'>
-                            <div className='email-text-top'>Username</div>
-                            <div className='email-text-bottom'>emailname2@gmail.com</div>
-                        </div>
-                    </div>
+            <div className='email'>
+                <div className='email-icon'>
+                    <FontAwesomeIcon icon={faUser} transform='grow-20' className='icon-user-secondary' />
+                </div>
+                    
+                <div className='email-text'>
+                    <div className='email-text-top'>Username</div>
+                    <div className='email-text-bottom'>emailname2@gmail.com</div>
+                </div>
             </div>
         );
     }
