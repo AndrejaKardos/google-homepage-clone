fbd9688438e4045ecdbba6929877e7ac00a71521
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-02 14:10:50 +1000

    Moved CSS code for Searchbar component to Searchbar.css

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faMicrophone } from '@fortawesome/free-solid-svg-icons'
-import '../App.css'
+import '../Searchbar.css'
 
 class Searchbar extends Component {
     render() {
