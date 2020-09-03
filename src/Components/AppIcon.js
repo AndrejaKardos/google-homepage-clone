84d2b28371f7f0b252db974c99119a23d79c69f5
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-03 16:24:04 +1000

    Added code to AppIcon component

---
+++ src/Components/AppIcon.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './AppIcon.css'
+
+class AppIcon extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const name = this.props.name;
+        const text = this.props.text;
+        return(
+            <div className='appicon'>
+                <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                    <FontAwesomeIcon icon={name} transform='grow-30' className='appicon__image'/>
+                    <div className='appicon__text'>
+                        {text}
+                    </div>
+                </a>
+            </div>
+        );
+    }
+}
+
+export default AppIcon
