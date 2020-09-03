7678307be0e3ed955d3843b3a834c401dab803af
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-03 11:45:35 +1000

    Moved some code to MenuFooter.js from UserMenu.js

---
+++ src/Components/MenuFooter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './UserMenu.css'
+
+class MenuFooter extends Component {
+    render() {
+        return (
+            <div id='user-menu-bottom'>
+                <a href='https://policies.google.com/privacy?hl=en' className='user-menu-bottom-links'>Privacy Policy</a>
+                <span className="dot-span" aria-hidden="true">•</span>
+                <a href='https://policies.google.com/terms?hl=en' className='user-menu-bottom-links'>Terms of Service</a>
+            </div>
+        );
+    }
+}
+
+export default MenuFooter
