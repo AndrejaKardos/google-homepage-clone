71537d6125cb238402ac6ebbe93f95e63c8f48b4
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-03 12:04:41 +1000

    Included SignOut component in UserMenu.js

--- src/Components/UserMenu.js
+++ src/Components/UserMenu.js
@@ -4,6 +4,7 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 import UserAccount from './UserAccount'
 import Email from './Email'
 import AddAccount from './AddAccount'
+import SignOut from './SignOut'
 import MenuFooter from './MenuFooter'
 import './UserMenu.css'
 
@@ -43,9 +44,7 @@ class UserMenu extends Component {
                             <AddAccount />
                         </a>
 
-                        <div id='usermenu__links--signout'>
-                            <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=act' id='sign-out'>Sign out of all accounts</a>
-                        </div>
+                        <SignOut />
                         <MenuFooter />
                     </div>
                 }
