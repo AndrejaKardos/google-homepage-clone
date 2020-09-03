ac8ec8196e57dd3a19d6cb5b7a2290fef0aac5c9
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-03 15:31:30 +1000

    Added links to apps in AppCollection

--- src/Components/AppCollection.js
+++ src/Components/AppCollection.js
@@ -7,23 +7,36 @@ class AppCollection extends Component {
     render() {
         return (
             <div className='apps'>
-                <div className='apps__icon apps__icon--user'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                <div className='apps__icon'>
+                    <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                        <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                    </a>
                 </div>
-                <div className='apps__icon apps__icon--user'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                <div className='apps__icon'>
+                    <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                        <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                    </a>
                 </div>
-                <div className='apps__icon apps__icon--user'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                <div className='apps__icon'>
+                    <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                        <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                    </a>
                 </div>
-                <div className='apps__icon apps__icon--user'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-30' />
+
+                <div className='apps__icon'>
+                    <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                        <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                    </a>
                 </div>
-                <div className='apps__icon apps__icon--user'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                <div className='apps__icon'>
+                    <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                        <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                    </a>
                 </div>
-                <div className='apps__icon apps__icon--user'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                <div className='apps__icon'>
+                    <a href='https://myaccount.google.com/?utm_source=OGB&tab=wk&utm_medium=app'>
+                        <FontAwesomeIcon icon={faUser} transform='grow-30' />
+                    </a>
                 </div>
             </div>
         );
