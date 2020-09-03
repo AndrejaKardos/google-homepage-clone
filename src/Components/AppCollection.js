5694c5e83c7f329902fb9943687ed36e60b2a53e
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-03 15:16:47 +1000

    Set up positioning for first row of apps in AppCollection.css

--- src/Components/AppCollection.js
+++ src/Components/AppCollection.js
@@ -9,13 +9,13 @@ class AppCollection extends Component {
             <div className='apps'>
                 <div className='apps__row'>
                     <div className='apps__row--icon'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-5' id='appsmenu__button--icon' />
+                    <FontAwesomeIcon icon={faUser} transform='grow-30' id='appsmenu__button--icon' />
                     </div>
                     <div className='apps__row--icon'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-5' id='appsmenu__button--icon' />
+                    <FontAwesomeIcon icon={faUser} transform='grow-30' id='appsmenu__button--icon' />
                     </div>
                     <div className='apps__row--icon'>
-                    <FontAwesomeIcon icon={faUser} transform='grow-5' id='appsmenu__button--icon' />
+                    <FontAwesomeIcon icon={faUser} transform='grow-30' id='appsmenu__button--icon' />
                     </div>
                 </div>
             </div>
