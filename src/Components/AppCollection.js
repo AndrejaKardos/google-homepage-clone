cc6c76238b5aa4a4e914c973811b90fa992d1b05
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-03 14:56:24 +1000

    Created skeleton for AppCollection component

---
+++ src/Components/AppCollection.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './AppCollection.css'
+
+class AppCollection extends Component {
+    render() {
+        return (
+            <div className='apps'>
+                <div className='apps__row'>
+                    Icon Icon Icon
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AppCollection
