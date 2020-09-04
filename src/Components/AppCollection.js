21f97d8946828de480a748e66d7634616416b486
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-04 11:31:13 +1000

    Added 'Translate' AppIcon to AppCollection.js

--- src/Components/AppCollection.js
+++ src/Components/AppCollection.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { faUser, faSearch, faMapMarkerAlt, faPlayCircle, 
          faPlay, faNewspaper, faEnvelopeOpenText, faVideo,
-         faAddressBook, faCloudDownloadAlt, faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+         faAddressBook, faCloudDownloadAlt, faCalendarAlt,
+         faLanguage } from '@fortawesome/free-solid-svg-icons'
 import AppIcon from './AppIcon'
 import './AppCollection.css'
 
@@ -12,14 +13,18 @@ class AppCollection extends Component {
                 <AppIcon name={faUser} text='Account' />
                 <AppIcon name={faSearch} text='Search' />
                 <AppIcon name={faMapMarkerAlt} text='Maps' />
+
                 <AppIcon name={faPlayCircle} text='YouTube' />
                 <AppIcon name={faPlay} text='Play' />
                 <AppIcon name={faNewspaper} text='News' />
+
                 <AppIcon name={faEnvelopeOpenText} text='Gmail' />
                 <AppIcon name={faVideo} text='Meets' />
                 <AppIcon name={faAddressBook} text='Contacts' />
+
                 <AppIcon name={faCloudDownloadAlt} text='Drive' />
                 <AppIcon name={faCalendarAlt} text='Calendar' />
+                <AppIcon name={faLanguage} text='Translate' />
             </div>
         );
     }
