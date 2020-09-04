e1c0f65183f04c7a84d69e45ced9aef9793717bc
AndrejaKardos <andreja.kardos@gmail.com>
2020-09-04 12:34:31 +1000

    Added 'Podcasts' icon to AppCollection

--- src/Components/AppCollection.js
+++ src/Components/AppCollection.js
@@ -5,7 +5,8 @@ import { faUser, faSearch, faMapMarkerAlt, faPlayCircle,
          faLanguage, faPhotoVideo, faTag, faFileAlt, 
          faFileExcel, faFile, faBook, faBlog, faQuoteRight,
          faLightbulb, faTablet, faChalkboardTeacher, 
-         faGlobeAsia, faBookmark, faLandmark, faAd } from '@fortawesome/free-solid-svg-icons'
+         faGlobeAsia, faBookmark, faLandmark, faAd,
+         faPodcast } from '@fortawesome/free-solid-svg-icons'
 import AppIcon from './AppIcon'
 import './AppCollection.css'
 
@@ -52,6 +53,7 @@ class AppCollection extends Component {
                 <AppIcon name={faLandmark} text='Art and Culture' />
 
                 <AppIcon name={faAd} text='Google Ads' />
+                <AppIcon name={faPodcast} text='Podcasts' />
             </div>
         );
     }
